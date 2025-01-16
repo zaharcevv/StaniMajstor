f2b9e6de318e4d06319d2c2df6d1039408663e70
Hristijan <hristijanzaharcev1@gmail.com>
2025-01-16 15:01:22 +0100

    FIrebase configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,23 @@
+        // Import the functions you need from the SDKs you need
+        import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+        import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-analytics.js";
+        // TODO: Add SDKs for Firebase products that you want to use
+        // https://firebase.google.com/docs/web/setup#available-libraries
+      
+        // Your web app's Firebase configuration
+        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+        const firebaseConfig = {
+          apiKey: "AIzaSyBkc4S8nJ0AaGL0jhhf4wImevzeU0IGYwc",
+          authDomain: "stanimajstor-4c444.firebaseapp.com",
+          projectId: "stanimajstor-4c444",
+          storageBucket: "stanimajstor-4c444.firebasestorage.app",
+          messagingSenderId: "275539088354",
+          appId: "1:275539088354:web:920a5dae6bb71538221fbb",
+          measurementId: "G-TMJ17CBH93"
+        };
+      
+        // Initialize Firebase
+        const app = initializeApp(firebaseConfig);
+        const analytics = getAnalytics(app);
+
+        export { app, analytics };
