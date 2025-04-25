5ff193c1ac93b96e7afd73d5b8884fc47ca13136
Hristijan <hristijanzaharcev1@gmail.com>
2025-04-25 22:28:31 +0200

    Set base path for GitHub Pages deployment

--- DoneRight/Frontend/vite.config.js
+++ DoneRight/Frontend/vite.config.js
@@ -5,6 +5,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/StaniMajstor/",
   plugins: [
     vue(),
     vueDevTools(),
